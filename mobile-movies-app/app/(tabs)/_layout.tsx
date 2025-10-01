7e103365de159592b968bd26521687dd0c2e0ffd
= <ahmadhossamo789@gmail.com>
2025-10-01 17:28:35 +0300

    logo added

--- mobile-movies-app/app/(tabs)/_layout.tsx
+++ mobile-movies-app/app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from "expo-router";
 
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ImageBackground, Text } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
 type Props = {
   name: string;
@@ -12,22 +12,37 @@ type Props = {
 };
 
 function TabBarItem({ name, icon, focused }: Props) {
-  return (
-    <ImageBackground
-      className="flex flex-row flex-1 w-full min-w-[112px] min-h-14 mt-4 justify-center items-center overflow-hidden"
-      source={images.highlight}
-    >
-      <Image source={icon} tintColor={"#151312"} className="size-5" />
-      <Text className="text-secondary text-base font-semibold ml-2">
-        {name}
-      </Text>
-    </ImageBackground>
-  );
+  if (focused) {
+    return (
+      <ImageBackground
+        className="min-w-[120px] flex flex-row flex-1 min-h-16 mt-4 items-center justify-center rounded-full overflow-hidden"
+        source={images.highlight}
+      >
+        <Image source={icon} tintColor={"#151312"} className="size-5" />
+        <Text className="text-secondary text-sm font-semibold ml-2">
+          {name}
+        </Text>
+      </ImageBackground>
+    );
+  } else {
+    return (
+      <View className="size-full flex justify-center items-center mt-4 rounded-full">
+        <Image source={icon} tintColor={"#a8b5db"} className="size-5" />
+      </View>
+    );
+  }
 }
 
 const TabsLayout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: styles.tabBarIcon,
+        tabBarStyle: styles.tabBar,
+        animation: "fade",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
@@ -76,3 +91,25 @@ const TabsLayout = () => {
 };
 
 export default TabsLayout;
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: "#0f0d23",
+    borderRadius: 50,
+    marginHorizontal: 20,
+    marginBottom: 36,
+    height: 52,
+    position: "absolute",
+    overflow: "hidden",
+    borderWidth: 1,
+    borderColor: "#0f0d23",
+  },
+
+  tabBarIcon: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
