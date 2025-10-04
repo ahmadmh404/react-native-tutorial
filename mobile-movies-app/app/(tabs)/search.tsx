182766a3367319376aade57cb637220cdfcd4939
= <ahmadhossamo789@gmail.com>
2025-10-04 12:34:19 +0300

    search page with error, loading, empty staes

--- mobile-movies-app/app/(tabs)/search.tsx
+++ mobile-movies-app/app/(tabs)/search.tsx
@@ -1,10 +1,118 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, FlatList, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+import { images } from "@/constants/images";
+import useFetch from "@/services/use-fetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/movie-card";
+import { useRouter } from "expo-router";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/search-bar";
 
 const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const {
+    data: movies,
+    loading: loadingSearch,
+    error: errorSearch,
+    refetch: searchMovies,
+    reset,
+  } = useFetch(() => fetchMovies(searchQuery), false);
+
+  const handleSearchQueryChange = (query: string) => {
+    setSearchQuery(query);
+  };
+
+  useEffect(() => {
+    const func = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await searchMovies();
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(func);
+    };
+  }, [searchQuery, searchMovies, reset]);
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 w-full absolute z-0"
+        resizeMode="cover"
+      />
+
+      <FlatList
+        data={movies}
+        className="px-5"
+        renderItem={({ item }) => <MovieCard movie={item} />}
+        keyExtractor={({ item }) => item.id.toString()}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 20,
+          marginHorizontal: 16,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListHeaderComponent={
+          <View className="w-full mt-20 flex justify-center items-center">
+            <Image
+              source={icons.logo}
+              className="w-12 h-10"
+              resizeMode="cover"
+            />
+
+            <View className="my-5">
+              <SearchBar
+                value={searchQuery}
+                onChangeText={handleSearchQueryChange}
+                placeholder="Search movies..."
+              />
+            </View>
+
+            {loadingSearch && (
+              <ActivityIndicator
+                size={"large"}
+                color={"#0000ff"}
+                className="my-3"
+              />
+            )}
+
+            {errorSearch && (
+              <Text className="text-red-500 my-3">
+                Error: {errorSearch.message}
+              </Text>
+            )}
+
+            {/* Display if everything is correct */}
+            {!errorSearch &&
+              !loadingSearch &&
+              searchQuery.trim() &&
+              movies.length > 1 && (
+                <Text className="text-lg font-bold text-white">
+                  Search results for{" "}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </View>
+        }
+        ListEmptyComponent={
+          !loadingSearch && !errorSearch ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim()
+                  ? "No search results found"
+                  : "Search for a movie"}
+              </Text>
+            </View>
+          ) : null
+        }
+      />
     </View>
   );
 };
--- mobile-movies-app/components/search-bar.tsx
+++ mobile-movies-app/components/search-bar.tsx
@@ -1,13 +1,20 @@
-import { View, Text, Image, TextInput } from "react-native";
+import { View, Image, TextInput } from "react-native";
 import React from "react";
 import { icons } from "@/constants/icons";
 
 interface Pops {
+  value?: string;
+  onChangeText?: (value: string) => void;
   onPress?: () => void;
   placeholder: string;
 }
 
-export default function SearchBar({ onPress, placeholder }: Pops) {
+export default function SearchBar({
+  value,
+  onChangeText,
+  onPress,
+  placeholder,
+}: Pops) {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
@@ -20,8 +27,8 @@ export default function SearchBar({ onPress, placeholder }: Pops) {
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChange={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor={"#a8b5db"}
         className="flex-1 ml-2 text-white"
       />
